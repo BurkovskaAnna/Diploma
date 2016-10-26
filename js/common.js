bd2605ea09922fda7362814b6e105102d1275261
Anna Burkovska <anuta871@gmail.com>
2016-10-26 20:48:24 +0300

    add html,css,js

--- js/common.js
+++ js/common.js
@@ -78,6 +78,35 @@ function showDivs(n) {
   dots[slideIndex-1].className += " number-green";
 }
 
+var slideIndex = 2;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("team__item-slider");
+  // var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 2}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //    dots[i].className = dots[i].className.replace(" number-green", "");
+  // }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " number-green";
+}
+
+
+
 
 
 
