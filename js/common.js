dac9d62f7fefc5501946384286d9e1adf791799f
Anna Burkovska <anuta871@gmail.com>
2016-10-27 17:40:33 +0300

    add css, html, js

--- js/common.js
+++ js/common.js
@@ -78,31 +78,22 @@ function showDivs(n) {
   dots[slideIndex-1].className += " number-green";
 }
 
-var slideIndex = 2;
-showDivs(slideIndex);
+var slideIndex = 1;
+showDiv(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDiv(n) {
+  showDiv(slideIndex += n);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
+function showDiv(n) {
   var i;
   var x = document.getElementsByClassName("team__item-slider");
-  // var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 2}
+  if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
-  // for (i = 0; i < dots.length; i++) {
-  //    dots[i].className = dots[i].className.replace(" number-green", "");
-  // }
   x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " number-green";
 }
 
 
