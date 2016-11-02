956ede3d1d86f42a9f50f00eacdb10d717581fff
Anna Burkovska <anuta871@gmail.com>
2016-11-02 15:22:24 +0200

    add css

--- js/common.js
+++ js/common.js
@@ -51,6 +51,7 @@ $(window).load(function() {
 
 }); 
 
+// (function () {
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -77,24 +78,27 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " number-green";
 }
+// })();
 
-var slideIndex = 1;
-showDiv(slideIndex);
+(function () {
+	var slideIndex = 1;
+	showDiv(slideIndex);
 
-function plusDiv(n) {
-  showDiv(slideIndex += n);
-}
+	function plusDiv(n) {
+	  showDiv(slideIndex += n);
+	}
 
-function showDiv(n) {
-  var i;
-  var x = document.getElementsByClassName("team__item-slider");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+	function showDiv(n) {
+	  var i;
+	  var x = document.getElementsByClassName("team__item-slider");
+	  if (n > x.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = x.length}
+	  for (i = 0; i < x.length; i++) {
+	     x[i].style.display = "none";
+	  }
+	  x[slideIndex-1].style.display = "block";
+	}
+})();
 
 
 
